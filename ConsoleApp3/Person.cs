d5bd8871a576c7dfe7e056fe7d51f1a37bc652ce
dawidbessman <dawid.bessman@jit.team>
2023-07-20 11:26:12 +0200

    1

--- ConsoleApp3/Person.cs
+++ ConsoleApp3/Person.cs
@@ -6,7 +6,7 @@ public class Person
     {
         this.Name = name;
     }
-
+    
     public double Weight { get; set; }
     public int Age { get; set; }
     public string Name { get; set; }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,35 +10,21 @@ internal class Program
     {
         Person p1 = new Person("Łukasz Drzewiecki");
         p1.Weight = 97;
-        p1.Age = 16;
+        p1.Age = 176;
 
         Person p2 = new Person("Dawid Bessman");
-        p2.Weight = 76;
+        p2.Weight = 75;
         p2.Age = 16;
+        
         if (p2.Age == p1.Age)
         {
             Console.WriteLine("waza tyle samo");
         }
-
-        p2.AmIHeavier(p1);
-        int ageDiff = p2.Age - p1.Age;
-        var message = ageDiff switch
-        {
-            0 => "Sa rowno starzy",
-            int a when a < 15 => "Sa rowno starzy",
-            int a => "Sa rowno starzy",
-            _ => "Sa rowno starzy"
-        };
-        
-        Console.WriteLine(message);
-
-        int diff =  p2.Age - p1.Age;
-
-            if (p2.Age < p1.Age)
+        else if (p2.Age < p1.Age);
         {
             Console.WriteLine( p2.Name + " jest lzejszy");
         }
-        if (p1.Age < p2.Age)
+        else if (p2.Age > p1.Age);
         {
             Console.WriteLine( p1.Name + " jest lzejszy");
         }
@@ -51,7 +37,7 @@ internal class Program
             Console.WriteLine("Są w tym samym wieku");
         }
 
-        p2.AmIHeavier(p1);
+        p2.AmIHeavier(p1); 
         if (p2.Age < p1.Age)
         {
             Console.WriteLine( p2.Name + " jest młodszy");
@@ -62,26 +48,5 @@ internal class Program
         }
         int rozwicaWieku = p1.Age - p2.Age;
         Console.WriteLine("roznica wieku po między Dawidem a Łukaszem wynosi " + rozwicaWieku);
-        
-        
-        List<Person> persons = new List<Person>();
-        bool isPersonPresent = persons.Contains(person);
-        Console.WriteLine(isPersonPresent.ToString());
-        
-        bool isPersonPresent = persons.Contains(person);
-        Console.WriteLine(isPersonPresent.ToString()););
-        person.weight = "300kg";
-        person.age = 60;
-        person.name = "Łukasz Drzewiecki";
     }
 }
-
-
-
-
-
-
-
-
-
-
