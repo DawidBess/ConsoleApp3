7e598dcee1c873a260ef80d2915ccfd2ab53f582
dawidbessman <dawid.bessman@jit.team>
2023-07-20 14:10:06 +0200

    Naprawa ifów

--- ConsoleApp3/Person.cs
+++ ConsoleApp3/Person.cs
@@ -8,19 +8,53 @@ public class Person
         this.Age = age;
         this.Weight = weight;
     }
-    
+
     public double Weight { get; set; }
     public int Age { get; set; }
     public string Name { get; set; }
-    
+
     public bool AmIHeavier(Person person)
     {
         return false;
     }
-    
+
     bool AmIOlder(Person person)
     {
         return false;
     }
-    
+
+    public void CompareWeight(Person p2)
+    {
+        if (this.Weight == p2.Weight)
+        {
+            Console.WriteLine("waza tyle samo");
+        }
+        else if (this.Weight < p2.Weight)
+        {
+            Console.WriteLine(this.Name + " jest lzejszy");
+        }
+        else if (this.Weight > p2.Weight)
+        {
+            Console.WriteLine(p2.Name + " jest LŻEJSZY");
+        }
+
+    }
+    public void ComparAge(Person p2)
+    {
+        if (this.Age == p2.Age)
+        {
+            Console.WriteLine("Są w tym samym wieku");
+        }
+        else if (this.Age < p2.Age)
+        {
+            Console.WriteLine(this.Name + " jest młodszy");
+        }
+        else if (this.Age > p2.Age)
+        {
+            Console.WriteLine(p2.Name + " jest młodszy");
+        }
+
+        int rozwicaWieku = p2.Age - this.Age;
+        Console.WriteLine("roznica wieku po między Dawidem a Łukaszem wynosi " + rozwicaWieku);
+    }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,43 +8,17 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Person p1 = new Person("Łukasz Drzewiecki", 176,97);
-        p1.Weight = 97;
-        p1.Age = 176;
+        Person p1 = new Person("Łukasz Drzewiecki", 176, 97);
 
-        Person p2 = new Person("Dawid Bessman",16,75 );
-        p2.Weight = 75;
-        p2.Age = 16;
+        Person p2 = new Person("Dawid Bessman", 16, 75);
         
-        if (p2.Age == p1.Age)
-        {
-            Console.WriteLine("waza tyle samo");
-        }
-        else if (p2.Age < p1.Age)
-        {
-            Console.WriteLine( p2.Name + " jest lzejszy");
-        }
-        else if (p2.Age > p1.Age)
-        {
-            Console.WriteLine( p1.Name + " jest lzejszy");
-        }
+        p1.CompareWeight(p2);
         
         double roznicaWagi = p1.Weight - p2.Weight;
         Console.WriteLine("roznca wagi " + roznicaWagi + "kg");
-
-        if (p2.Age == p1.Age)
-        {
-            Console.WriteLine("Są w tym samym wieku");
-        }
-        else if (p2.Age < p1.Age)
-        {
-            Console.WriteLine( p2.Name + " jest młodszy");
-        }
-        else if (p1.Age < p2.Age)
-        {
-            Console.WriteLine( p1.Name + " jest młodszy");
-        }
-        int rozwicaWieku = p1.Age - p2.Age;
-        Console.WriteLine("roznica wieku po między Dawidem a Łukaszem wynosi " + rozwicaWieku);
+        
+        p2.ComparAge(p1);
     }
 }
+    
+    
